e35df8734b7c675f95e55677fa3efe5982dcb405
Josh Varty <joshvarty@gmail.com>
2018-11-13 22:14:32 -0500

    Begin generating rollouts

--- game_engine/agent.cpp
+++ game_engine/agent.cpp
@@ -5,6 +5,15 @@
 
 namespace hlt {
 
+struct rollout_item {
+    int state[64*64*17];
+    int action[6];
+    double value;
+    double log_prob; //TODO: Change to Tensor
+    double reward;
+    int done;
+};
+
 Agent::Agent(HaliteImpl &game, int stateWidthHeight, int stateDepth, int actionSize)
 : game(game)
 {
@@ -16,6 +25,10 @@ void Agent::step(){
 }
 
 void Agent::generate_rollout() {
+    std::vector<rollout_item> rollout;
+    //TODO: Set up list of episode rewards
+    //TODO: Reset environment for new game
+    
     return;
 }
 
--- game_engine/agent.hpp
+++ game_engine/agent.hpp
@@ -7,6 +7,13 @@ namespace hlt {
 class Agent {
 
     HaliteImpl game;
+    double discount_rate = 0.99;        //Amount by which to discount future rewards
+    double tau = 0.95;                  //
+    int learningRounds = 10;            //number of optimization rounds for a single rollout
+    int mini_batch_number = 32;         //batch size for optimization 
+    double ppo_clip = 0.2;              //
+    int gradient_clip = 5;              //Clip gradient to try to prevent unstable learning
+    int maximum_timesteps = 1200;       //Maximum timesteps over which to generate a rollout
 
     void generate_rollout();
 
