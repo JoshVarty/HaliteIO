475f9ecc7d7c4d09b093ad0f144cc8bf1dab3646
Josh Varty <joshvarty@gmail.com>
2018-11-14 14:25:19 -0500

    Begin main loop

--- game_engine/core/Halite.cpp
+++ game_engine/core/Halite.cpp
@@ -3,8 +3,94 @@
 
 #include "Halite.hpp"
 #include "HaliteImpl.hpp"
-
+//
 namespace hlt {
+
+    
+
+struct rollout_item {
+    int state[64*64*17];
+    int action[6];
+    double value;
+    double log_prob; //TODO: Change to Tensor
+    double reward;
+    int done;
+};
+
+Agent::Agent()
+{
+}
+
+double Agent::step(){
+    return 0;
+}
+
+std::unique_ptr<hlt::Halite> reset_game(){
+    // int map_width = 64;
+    // int map_height = 64;
+    // int numPlayers = 2;
+    // hlt::mapgen::MapType type = mapgen::MapType::Fractal;
+    // auto seed = static_cast<unsigned int>(time(nullptr));
+    // hlt::mapgen::MapParameters map_parameters{type, seed, map_width, map_height, numPlayers};
+    // hlt::Map map(map_width, map_height);
+    // hlt::mapgen::Generator::generate(map, map_parameters);
+    // std::string replay_directory = "replays/";
+    // constexpr auto SEPARATOR = '/';
+    // if (replay_directory.back() != SEPARATOR) replay_directory.push_back(SEPARATOR);
+    // auto game_statistics = std::make_unique<hlt::GameStatistics>();
+    // auto replay = std::make_unique<hlt::Replay>(*game_statistics, map_parameters.num_players, map_parameters.seed, map);
+
+    // auto game = std::make_unique<hlt::Halite>(map, *game_statistics, *replay);
+    // return game;
+}
+
+
+
+void Agent::generate_rollout() {
+    // std::vector<rollout_item> rollout;
+    // //TODO: Set up list of episode rewards
+    // //Reset environment for new game
+    // auto game_unique_ptr = reset_game();
+    // auto game_ptr = game_unique_ptr.get();
+
+    // auto grid = game_ptr->map.grid;
+
+    return;
+}
+
+void Agent::process_rollout(){
+    return;
+}
+
+void Agent::train_network(){
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 /**
  * Constructor for the main game.
--- game_engine/core/Halite.hpp
+++ game_engine/core/Halite.hpp
@@ -65,6 +65,37 @@ public:
     ~Halite();
 };
 
+
+
+
+
+class Agent {
+
+    double discount_rate = 0.99;        //Amount by which to discount future rewards
+    double tau = 0.95;                  //
+    int learningRounds = 10;            //number of optimization rounds for a single rollout
+    int mini_batch_number = 32;         //batch size for optimization 
+    double ppo_clip = 0.2;              //
+    int gradient_clip = 5;              //Clip gradient to try to prevent unstable learning
+    int maximum_timesteps = 1200;       //Maximum timesteps over which to generate a rollout
+
+    void generate_rollout();
+
+    void process_rollout();
+
+    void train_network();
+
+public:
+
+    /**
+     * Construct Agent from state size and action size
+     *
+     */
+    explicit Agent();
+
+    double step();
+};
+
 }
 
 #endif
