7470b44e5e7ceaf4bc1fd4a5cbda1c7764118b60
Josh Varty <joshvarty@gmail.com>
2018-12-18 00:25:46 -0500

    Init bottleneck/basicblock

--- model.hpp
+++ model.hpp
@@ -4,9 +4,94 @@
 #include <torch/torch.h>
 #include "types.hpp"
 
+torch::nn::Conv2d conv3x3(int64_t inputChannels, int64_t outputChannels) {
+    return torch::nn::Conv2d(torch::nn::Conv2dOptions(inputChannels, outputChannels, /*kernel_size=*/3));
+}
+
+torch::nn::Conv2d conv1x1(int64_t inputChannels, int64_t outputChannels) {
+    return torch::nn::Conv2d(torch::nn::Conv2dOptions(inputChannels, outputChannels, /*kernel_size=*/1));
+}
+
+struct BasicBlock : torch::nn::Module {
+public:
+    torch::nn::Conv2d conv1;
+    torch::nn::BatchNorm bn1;
+    torch::nn::Conv2d conv2;
+    torch::nn::BatchNorm bn2;
+
+    BasicBlock(int64_t inplanes, int64_t planes)
+    :   conv1(conv3x3(inplanes, planes)),
+        bn1(torch::nn::BatchNorm(torch::nn::BatchNormOptions(planes))),
+        conv2(conv3x3(planes, planes)),
+        bn2(torch::nn::BatchNorm(torch::nn::BatchNormOptions(planes)))
+    {
+    }
+
+    torch::Tensor forward(torch::Tensor x, torch::Tensor selected_action) {
+        auto identity = x;
+
+        auto out = conv1->forward(x);
+        out = bn1->forward(out);
+        out = torch::relu(out);
+        out = conv2->forward(out);
+        out = bn2->forward(out);
+
+        //TODO: Downsample??
+        //  if self.downsample is not None:
+        //     identity = self.downsample(x)
+
+        out = out + identity;
+        out = torch::relu(out);
+        return out;
+    }
+};
+
+struct Bottleneck : torch::nn::Module {
+    torch::nn::Conv2d conv1;
+    torch::nn::BatchNorm bn1;
+    torch::nn::Conv2d conv2;
+    torch::nn::BatchNorm bn2;
+    torch::nn::Conv2d conv3;
+    torch::nn::BatchNorm bn3;
+
+    Bottleneck(int64_t inplanes, int64_t planes)
+    :   conv1(conv1x1(inplanes, planes)),
+        bn1(torch::nn::BatchNorm(torch::nn::BatchNormOptions(planes))),
+        conv2(conv3x3(planes, planes)),
+        bn2(torch::nn::BatchNorm(torch::nn::BatchNormOptions(planes))),
+        conv3(conv1x1(planes, planes * 4)),
+        bn3(torch::nn::BatchNorm(torch::nn::BatchNormOptions(planes * 4)))
+    {
+    }
+
+    torch::Tensor foward(torch::Tensor x) {
+        auto identity = x;
+        
+        auto out = conv1->forward(x);
+        out = bn1->forward(out);
+        out = torch::relu(out);
+
+        out = conv2->forward(out);
+        out = bn2->forward(out);
+        out = torch::relu(out);
+
+        out = conv3->forward(out);
+        out = bn3->forward(out);
+
+        //TODO Downsample
+        // if self.downsample is not None:
+        //     identity = self.downsample(x)
+
+        out = out + identity;
+        out = torch::relu(out);
+        return out;
+    }
+};
 
 struct ActorCriticNetwork : torch::nn::Module {
 public:
+
+
 
     ActorCriticNetwork(bool training)
     :   conv1(torch::nn::Conv2dOptions(NUMBER_OF_FRAMES, 32, /*kernel_size=*/7)),
